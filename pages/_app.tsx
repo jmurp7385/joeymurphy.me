11f65095072c240fa25d5c3506b23f3d5a0f8069
jmurp7385 <jmurp7385@gmail.com>
2022-12-11 20:46:45 -0600

    fix typings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
+import { FunctionComponent } from 'react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: {Component: FunctionComponent, pageProps: Record<string, unknown>}) {
   return <Component {...pageProps} />
 }
 

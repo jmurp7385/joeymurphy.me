f3282e5a81bfda56221d05a88a2236f421e9a11b
Joey Murphy <jmurp7385@gmail.com>
2023-12-28 22:57:10 -0600

    add vercel analytics (#30)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import { FunctionComponent } from 'react'
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: {Component: FunctionComponent, pageProps: Record<string, unknown>}) {
-  return <Component {...pageProps} />
+  return <>
+  <Component {...pageProps} />
+  <Analytics />
+  </>
 }
 
 export default MyApp

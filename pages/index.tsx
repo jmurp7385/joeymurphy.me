fb83b5da1d495cac4d6c25f5300090d1ef17fc2c
Joey Murphy <jmurp7385@gmail.com>
2023-12-28 22:33:53 -0600

    break up home text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { NextSeo } from "next-seo";
-import Footer from '../components/Footer';
-import Link from 'next/link'
+import Footer from "../components/Footer";
+import Link from "next/link";
 
 const SEO = {
   title: "Joey Murphy",
   description: "Homepage of Joey Murphy",
   url: "https://joeymurphy.me",
-}
+};
 
 export default function Home() {
   return (
@@ -30,11 +30,12 @@ export default function Home() {
         <p className={styles.description}>
           <a href='https://github.com/jmurp7385'>Software Engineer</a>
           &nbsp;&&nbsp;
-          <a href='https://www.instagram.com/joeymurphy.photography/' >
+          <a href='https://www.instagram.com/joeymurphy.photography/'>
             Photographer
           </a>
-          .&nbsp;
-          <Link href="/reading-lists">See what I have been reading!</Link>
+        </p>
+        <p className={styles.description} style={{marginTop: 0}}>
+          <Link href='/reading-lists'>See what I have been reading!</Link>
         </p>
       </main>
       <Footer />
--- pages/reading-lists/2022.tsx
+++ pages/reading-lists/2022.tsx
@@ -1,26 +1,28 @@
 import styles from "../../styles/ReadingLists.module.css";
 import Footer from "../../components/Footer";
 import commonStyles from "../../styles/common.module.css";
-import Breadcrumbs, { tranformLabelHuman } from '../../components/Breadcrumbs';
+import Breadcrumbs, { tranformLabelHuman } from "../../components/Breadcrumbs";
 
 const bookList = [
-{title: 'Sword of Destiny',link: '',},
-{title: 'Blood of Elves',link: '',},
-{title: 'Time of contempt',link: '',},
-{title: 'Baptism of Fire',link: '',},
-{title: 'Tower of Swallows',link: '',},
-{title: 'Lady of the Lake',link: '',},
-{title: 'Season of Storms',link: '',},
-{title: 'She comes first',link: '',},
-{title: 'Fingerprints of the gods',link: '',},
-{title: 'Magicians of the Gods',link: '',},
+  { title: "Sword of Destiny", link: "" },
+  { title: "Blood of Elves", link: "" },
+  { title: "Time of contempt", link: "" },
+  { title: "Baptism of Fire", link: "" },
+  { title: "Tower of Swallows", link: "" },
+  { title: "Lady of the Lake", link: "" },
+  { title: "Season of Storms", link: "" },
+  { title: "She comes first", link: "" },
+  { title: "Fingerprints of the gods", link: "" },
+  { title: "Magicians of the Gods", link: "" },
 ];
 
 export default function ReadingList2022() {
   return (
     <main className={styles.main}>
-            <Breadcrumbs containerStyle={{alignSelf: 'flex-start', paddingBottom: '2.5rem'}} transformLabel={tranformLabelHuman}/>
-
+      <Breadcrumbs
+        containerStyle={{ alignSelf: "flex-start", paddingBottom: "2.5rem" }}
+        transformLabel={tranformLabelHuman}
+      />
       <h1 className={styles.title}>Reading List 2022</h1>
       <p className={styles.description}>Ordered by completion</p>
       <ol className={styles.container}>

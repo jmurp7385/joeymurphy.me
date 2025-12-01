61b659558dfcdd2b7713bdf406bb96bcc95e30fe
jmurp7385 <jmurp7385@gmail.com>
2025-11-30 18:29:05 -0600

    updates

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['@iconify/react', '@iconify-icons/cib'],
-  experimental: {
-    reactCompiler: true,
-  },
+  reactCompiler: true,
 };
 
 export default nextConfig;

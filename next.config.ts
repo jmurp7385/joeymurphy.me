500ed2e56c413130a764803b65c7de0b16133f6f
Joey Murphy <jmurp7385@gmail.com>
2026-07-28 19:55:47 -0500

    chore - update tooling

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['@iconify/react', '@iconify-icons/cib'],
   reactCompiler: true,
+  reactStrictMode: true,
+  // Add the following to fix the error with styled-jsx in Turbopack
+  turbopack: {
+    resolveAlias: {
+      // This is the specific alias needed to resolve the issue.
+      'styled-jsx/style': 'styled-jsx/style.js',
+    },
+  },
 };
 
 export default nextConfig;
